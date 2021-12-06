cd031916b892b9c91e74474e8f003b43b012031a
Matthieu Rochette <matthieu.rochette@epitech.eu>
2021-12-07 00:48:10 +0100

    try fix deployment by removing checkpoints

--- deep_learning_fashion_mnist_dataset_epitech.py
+++ deep_learning_fashion_mnist_dataset_epitech.py
@@ -105,9 +105,9 @@ tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
 checkpoint_path = "training_saves/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 # Create a callback that saves the model's weights
-cp_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_path, save_weights_only=True, verbose=1
-)
+# cp_callback = tf.keras.callbacks.ModelCheckpoint(
+#     filepath=checkpoint_path, save_weights_only=True, verbose=1
+# )
 
 
 def fit_model(model, epochs: int = 20):
@@ -116,7 +116,8 @@ def fit_model(model, epochs: int = 20):
         train_images,
         train_labels,
         epochs=20,
-        callbacks=[tensorboard_callback, cp_callback],
+        callbacks=[tensorboard_callback],
+        # callbacks=[tensorboard_callback, cp_callback],
     )
     return model
 
